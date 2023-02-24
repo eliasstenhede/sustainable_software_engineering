36303a0c115d06e7e793c9eb55072d739c2bd5cc
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2023-02-24 14:32:56 +0100

    change squared comparison

--- point_dist.py
+++ point_dist.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 if __name__=="__main__":
-    mat = np.loadtxt("./positions.xyz")
+    mat = np.loadtxt("./data/positions.xyz")
     mat = mat[mat[:, 0].argsort()[::-1]]
     n_lines = mat.size//3
 
@@ -10,6 +10,6 @@ if __name__=="__main__":
         for jx in range(ix+1, n_lines):
             if mat[ix,0]-mat[jx,0] > 0.05:
                 break
-            if np.linalg.norm(mat[ix]-mat[jx])**2 < 0.05**2:
+            if np.linalg.norm(mat[ix]-mat[jx]) < 0.05:
                 count+=1
     print("close points:", count)
