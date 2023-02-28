9bbe8ba6530d343e613d6793f54ce7daac9a2de3
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2023-02-28 17:04:37 +0100

    now warms up computer and deletes old benchmarks before starting up

--- runfiles.bat
+++ runfiles.bat
@@ -1,4 +1,14 @@
-for /l %%i in (1, 1, 20) do (
+rmdir /s /q benchmarks
+
+for /l %%i in (1, 1, 30) do (
+  run.bat point_dist.exe C
+  run.bat "py point_dist.py" Python
+  run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+  rmdir /s /q benchmarks
+)
+
+
+for /l %%i in (1, 1, 30) do (
 	run.bat point_dist.exe C
 	run.bat "py point_dist.py" Python
 	run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
