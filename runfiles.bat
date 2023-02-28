0b1c8777953edccbbfade577d2bcb954baf5fa3e
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2023-02-28 22:06:56 +0100

    no need to warm up for this long...

--- runfiles.bat
+++ runfiles.bat
@@ -1,6 +1,6 @@
 rmdir /s /q benchmarks
 
-for /l %%i in (1, 1, 30) do (
+for /l %%i in (1, 1, 1) do (
   run.bat point_dist.exe C
   run.bat "py point_dist.py" Python
   run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
