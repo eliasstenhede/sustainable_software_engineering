ca3c915ae494788f5b733cc87d8a8840118ab8d3
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2023-02-28 22:29:42 +0100

    run the test 30 times

--- runfiles.bat
+++ runfiles.bat
@@ -1,13 +1,4 @@
 rmdir /s /q benchmarks
-
-for /l %%i in (1, 1, 1) do (
-  run.bat point_dist.exe C
-  run.bat "py point_dist.py" Python
-  run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
-  rmdir /s /q benchmarks
-)
-
-
 for /l %%i in (1, 1, 30) do (
 	run.bat point_dist.exe C
 	run.bat "py point_dist.py" Python
