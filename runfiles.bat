3e2cb76b2ece9fd892af3c08c7e7498d8c4bfd83
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2023-03-01 17:52:25 +0100

    Gustavs randomly generated one

--- runfiles.bat
+++ runfiles.bat
@@ -1,6 +1,91 @@
 rmdir /s /q benchmarks
-for /l %%i in (1, 1, 30) do (
-	run.bat point_dist.exe C
-	run.bat "py point_dist.py" Python
-	run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
-)
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat "py point_dist.py" Python
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "py point_dist.py" Python
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat point_dist.exe C
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat "matlab -nodesktop -noawt -batch point_dist;exit;" Matlab
+run.bat point_dist.exe C
